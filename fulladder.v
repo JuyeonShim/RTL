ad00041571c5e402da7a1f79722eb1d28e08cc1b
JuyeonShim <138649341+JuyeonShim@users.noreply.github.com>
2024-05-22 18:02:43 +0900

    Update fulladder.v

--- fulladder.v
+++ fulladder.v
@@ -5,20 +5,6 @@ module fulladder(a, b, c_in, s, c_out);
 
 wire s1, c1, s2;
 
-// assign s1 = a ^ b;
-
-// always를 쓰기 위해서는 좌변이 reg로 선언되어 있어야함.
-
-// wire s1; -> reg s1;
-
-// always @(*) begin
-//     s1 = a ^ b;
-// end
-
-// always @(a, b) begin
-//     s1 = a ^ b;
-// end
-
 xor(s1, a, b);
 
 xor(s, s1, c_in);
